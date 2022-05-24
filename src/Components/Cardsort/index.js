a3dbc41dde9da9d8fe2946b49f8d293907a2bcc9
Tallia-Anne <erinawambiekele@gmail.com>
2022-05-24 09:26:15 +0200

    changer le titre du document

--- src/Components/Cardsort/index.js
+++ src/Components/Cardsort/index.js
@@ -1,12 +1,17 @@
 import './index.css'
 
-
+// animation Aos
+import Aos from 'aos';
+import "aos/dist/aos.css"
+import { useEffect } from 'react';
 
 const Cardsort = (props) => {
-    
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, [])
     return (
         <>
-        <div className="card-sorts">
+            <div className="card-sorts" data-aos="fade"  >
                
                 <div className="card-content-sort">
                     
---
+++ src/Components/Loading/loading.js
@@ -0,0 +1,15 @@
+import ReactLoading from 'react-loading';
+
+const Loading = () => {
+    
+    
+    return (
+        <ReactLoading
+            type='cylon'
+            color='#bada55'
+        />
+)
+    
+}
+
+export default Loading
